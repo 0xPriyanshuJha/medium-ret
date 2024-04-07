b99e37dcbbc20803e6438d41ab20c493b86c42de
Priyanshu0931 <priyanshujha2002@gmail.com>
2024-04-08 03:27:24 +0530

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"; 
 import Header from "./components/header";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
